53a4d5151e44fff547b78feec8eaaf065e392b6c
Bashsoft707 <alausabashir@gmail.com>
2024-01-01 14:43:16 +0100

    push

--- src/App.tsx
+++ src/App.tsx
@@ -5,75 +5,14 @@ import { LoginPage, MainPage, SignupPage } from "./pages";
 
 function App() {
   return (
-    // <div className="min-h-full h-screen">
-    //   <div className="max-w-md w-full space-y-8">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/" element={<MainPage />} />
-          </Routes>
-        </BrowserRouter>
-    //   </div>
-    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-// const AuthLayout = () => (
-//   <AuthContextProvider>
-//     <Outlet />
-//     <ApiErrorWatcher />
-//   </AuthContextProvider>
-// );
-
-// export const router = createBrowserRouter([
-//   {
-//     path: 'register',
-//     element: <Registration />,
-//   },
-//   {
-//     path: 'forgot-password',
-//     element: <RequestPasswordReset />,
-//   },
-//   {
-//     path: 'reset-password',
-//     element: <ResetPassword />,
-//   },
-//   {
-//     element: <AuthLayout />,
-//     children: [
-//       {
-//         path: 'login',
-//         element: <Login />,
-//       },
-//       {
-//         path: '/',
-//         element: <Root />,
-//         children: [
-//           {
-//             index: true,
-//             element: <Navigate to="/c/new" replace={true} />,
-//           },
-//           {
-//             path: 'c/:conversationId?',
-//             element: <ChatRoute />,
-//           },
-//           {
-//             path: 'chat/:conversationId?',
-//             element: <Chat />,
-//           },
-//           {
-//             path: 'a/:conversationId?',
-//             element: <AssistantsRoute />,
-//           },
-//           {
-//             path: 'search/:query?',
-//             element: <Search />,
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ]);
